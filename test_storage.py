bb7d906ca804919098ac55cf73968c58b74c3587
Udi Oron <udioron@gmail.com>
2023-11-12 11:46:22 +0200

    better add_item test

--- test_storage.py
+++ test_storage.py
@@ -1,10 +1,14 @@
 from storage import Storage
 
 
-def test_add_coffee():
+def test_add_coffee_and_milk():
     db = Storage("shopping_cart_test", clear=True)
     doc = db.add_item(12345, "coffee")
     assert doc['items'] == ['coffee']
+    doc = db.add_item(12345, "milk")
+    assert doc['items'] == ['coffee', 'milk']
+    doc = db.add_item(12345, "coffee")
+    assert doc['items'] == ['coffee', 'milk']
 
 
 
